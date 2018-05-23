19f5420316d9b98f4bf08dc165a3ce9cb5545d0d
Cris Wilson <cris.wilson77@gmail.com>
2018-05-23 11:55:40 +0100

    Frist

---
+++ MockingTools/MockSet.cs
@@ -0,0 +1,20 @@
+﻿using NSubstitute;
+using System.Data.Entity;
+using System.Linq;
+
+namespace MockingTools
+{
+    public class MockSet<T> where T : class
+    {
+        public DbSet<T> Create(IQueryable<T> mockData)
+        {
+            var mockSet = Substitute.For<DbSet<T>, IQueryable<T>>();
+
+            ((IQueryable<T>)mockSet).Provider.Returns(mockData.Provider);
+            ((IQueryable<T>)mockSet).Expression.Returns(mockData.Expression);
+            ((IQueryable<T>)mockSet).ElementType.Returns(mockData.ElementType);
+            ((IQueryable<T>)mockSet).GetEnumerator().Returns(mockData.GetEnumerator());
+            return mockSet;
+        }
+    }
+}
